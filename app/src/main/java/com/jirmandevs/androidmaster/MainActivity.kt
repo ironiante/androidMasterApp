44a615235a00130b87fb245607cc3aa4a4d21c96
jirma <jirman.rodriguez@uptc.edu.co>
2023-04-12 15:58:42 -0500

    editx y button , mensajes en el logcat

--- app/src/main/java/com/jirmandevs/androidmaster/MainActivity.kt
+++ app/src/main/java/com/jirmandevs/androidmaster/MainActivity.kt
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btnStart = findViewById<AppCompatButton>(R.id.btnStar) //creamos una variable <tipo de vista>, ya esta enganchado el boton.
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+
+
         btnStart.setOnClickListener {// cuando pulse el boton va hacer lo que dice entre llaves
-            Log.i("jirman ","Button pulsado") //
+            //creamos una variable
+            val name = etName.text.toString()
+            if (name.isNotEmpty()){
+                Log.i("jirman ","Button pulsado $name") //
+            }
+
 
         }
     }
